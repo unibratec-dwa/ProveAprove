0e247977cb31e730f98c4029d932162380f71e5e
Dennis Calazans <denniscalazans@gmail.com>
2014-07-12 13:18:10 -0300

    Adequação do resultado em JSON

--- receita.php
+++ receita.php
@@ -5,25 +5,45 @@ header("Content-type: application/json; charset=utf-8");
 //1 - Conexão
 include("inc.conexao.php");
 
-//2 - Consulta/Query
-if(!is_numeric($_GET['id'])) {
-	echo "Consulta inválida"; 
-	exit;
-}
-
-$id = $_GET['id'];
-$query = "select * from receita where id = ".$id;
-$resultado = mysqli_query($conexao,$query);
-$totalDeResultados = mysqli_num_rows($resultado);
-
 $json = array();
 $json['dados'] = array();
 
+if(!isset($_GET['id'])) {
+	$json['status'] = FALSE;
+	$json['message'] = "O id da receita não foi informado";
+	echo json_encode($json);
+	exit;
+} 
+
+$id = $_GET['id'];
+$query = "select u.nome as 'autor', r.* from receita r, usuario u where r.email = u.email AND id = ".$id;
+
+$resultado = mysqli_query($conexao,$query);
+$totalDeResultados = mysqli_num_rows($resultado);
+
 if($totalDeResultados > 0) {
 	$json['status'] = TRUE;
+	
 	while($receita = mysqli_fetch_object($resultado)) {
 		//4 - Apresentação do resultado
+
+		$receita->ingredientes = array();
+
+		$consultaIngredientes = "SELECT i.titulo, ri.quantidade 
+			FROM ingrediente i, receita_ingrediente ri
+			WHERE receita_id = $id AND i.id = ri.ingrediente_id";
+
+		$resultadoIngredientes = mysqli_query($conexao, $consultaIngredientes);
+		$totalDeIngredientes = mysqli_num_rows($resultadoIngredientes);
+		
+		if($totalDeIngredientes > 0) {
+			while($ingrediente = mysqli_fetch_object($resultadoIngredientes)) {
+				$receita->ingredientes[] = $ingrediente;
+			}
+		}
+
 		$json['dados'][] = $receita;
+
 	}
 
 } else {
