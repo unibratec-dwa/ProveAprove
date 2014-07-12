dc58b8aac5c94d1320a35d6d5c860a471847b3a6
Dennis Calazans <denniscalazans@gmail.com>
2014-07-12 13:24:10 -0300

    Inclusão do arquivo base de categorias

---
+++ categorias.php
@@ -0,0 +1,19 @@
+<?php
+
+//Definir o tipo de conteúdo no cabeçalho como JSON
+
+
+// Iniciar a conexão
+
+
+//Definir a consulta por todas as categorias em ordem alfabética decrescente
+
+
+//Apresentar os resultados em JSON
+
+/**
+ * Lembrar de apresentar a propriedade status, message (se o status for FALSE) e a propriedade dados como array
+ */
+
+
+?>
