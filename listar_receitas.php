6d80a730798fd66efd72518b0cf463f1712dec95
Dennis Calazans <denniscalazans@gmail.com>
2014-09-13 15:58:41 -0300

    Adiciona o arquivo de listagem de receitas por filtro

--- listar_receitas.php
+++ listar_receitas.php
@@ -1,25 +1,21 @@
 <?php
 header("Content-type: application/json; charset=utf-8");
-//Conexão com o banco de dados
 
-$host = "localhost";
-$usuario = "root";
-$senha = "123456";
-$banco = "ProveAprove";
+include("inc.conexao.php");
 
-$conexao = mysqli_connect($host, $usuario, $senha, $banco);
-mysqli_query($conexao, "SET NAMES 'utf8';");
-
-// if(!$conexao) {
-// 	echo "Não foi possível conectar ao banco de dados"; 
-// } else {
-// 	echo "Conexão OK";
-// }
-
+//Filtro de receitas
 $filtro = NULL;
+
+//Id para filtro
 $id = NULL;
+
+//Objeto de retorno
 $json = array();
+
+//Inicia falso
 $json['status'] = FALSE;
+
+//Inicial sem dados
 $json['dados'] = array();
 
 
@@ -27,40 +23,54 @@ if(isset($_GET['id'])) {
 	$id = $_GET['id'];
 }
 
+//Últimas receitas
+//Se nenhum filtro for apresentado ou se o filtro for "ultimas"
 if(!isset($_GET['filtro']) || $_GET['filtro'] == "ultimas") {
 	$json['status'] = TRUE;
-	$query = "SELECT 
-				r.id, 
-				r.titulo, 
-				u.nome 	as 'autor', 
-				c.titulo	as 'categoria', 
-				IFNULL(ROUND(AVG(ur.nota),1),0)	as 'media',
-				IFNULL(SUM(ur.gostou),0) 		as 'gostou', 
-				IFNULL(SUM(ur.favorito),0) 	as 'favorita'
-				FROM 
-				receita r
 
-				INNER JOIN usuario u 
-				ON r.email = u.email 
+	$query = "SELECT r.id, r.titulo, u.nome as 'autor', c.titulo as 'categoria', IFNULL(ROUND(AVG(ur.nota),1),0) as 'media',IFNULL(SUM(ur.gostou),0) as 'gostou', IFNULL(SUM(ur.favorito),0) as 'favorita'
+				FROM receita r
+				INNER JOIN usuario u ON r.email = u.email 
+				INNER JOIN categoria c ON r.categoria_id = c.id
+				LEFT JOIN usuario_receita ur  ON r.id = ur.receita_id
+				GROUP BY r.id ORDER BY r.id DESC";
 
-				INNER JOIN categoria c 
-				ON r.categoria_id = c.id
+} 
 
-				LEFT JOIN usuario_receita ur 
-				ON r.id = ur.receita_id
+//Com filtro
+else { $filtro = $_GET['filtro'];
+	
+	if($filtro=="favoritos") {
 
-				GROUP BY r.id
+		if($id == NULL) {
+			$json['mensagem'] = 'Usuário não informado';
+		} else {
+			$json['status'] = TRUE;
 
-				ORDER BY r.id DESC";
-} else {
-	$filtro = $_GET['filtro'];
+			$query = "SELECT r.id, r.titulo, u.nome AS  'autor', c.titulo AS  'categoria'
+FROM receita r
+INNER JOIN usuario u ON r.email = u.email
+INNER JOIN categoria c ON r.categoria_id = c.id
+RIGHT JOIN usuario_receita ur ON r.id = ur.receita_id
+WHERE ur.email =  '$id'
+AND ur.favorito =1
+ORDER BY categoria, titulo";
+		}
 
+	}
+
+
+	//Listar por categorias
 	if($filtro == "categoria") {
 		
 		if($id == NULL) {
+			
 			$json['mensagem'] = "Nenhuma categoria foi selecionada";
+		
 		} else { 
+			
 			$json['status'] = TRUE;
+
 			$query = "SELECT 
 						r.id, 
 						r.titulo, 
